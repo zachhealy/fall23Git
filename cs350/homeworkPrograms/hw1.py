a876f5eaa48ea117f4cdefd26290df045c741925
zachhealy <zachjhealy@gmail.com>
2023-08-29 13:15:52 -0500

    commit on 'main'

---
+++ cs350/homeworkPrograms/hw1.py
@@ -0,0 +1,23 @@
+import math
+'''
+n = 1000
+
+print(math.log(n))
+print(n)
+print(n**2)
+print(n**3)
+print(2**n)
+'''
+
+'''
+n = 1
+five = 0
+twofive = 1
+
+while twofive > five:
+    n+=1
+    twofive = 25 * n * math.log(n)
+    five = 5 * (n ** 2)
+    print(n)
+'''
+
