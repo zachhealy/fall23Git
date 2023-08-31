69f9ef28ce59749f83d807836b0ac3e9c8622cdf
zachhealy <zachjhealy@gmail.com>
2023-08-31 10:02:36 -0500

    testing more stuff

--- cs390/java/craps2.java
+++ cs390/java/craps2.java
@@ -14,11 +14,13 @@ public class craps2 {
             System.out.println("Balance: $" + balance);
             System.out.println("1. Bet on Pass");
             System.out.println("2. Bet on Don't Pass");
-            System.out.println("3. Quit");
+            System.out.println("3. Bet on Come");
+            System.out.println("4. Bet on Don't Come");
+            System.out.println("5. Quit");
             System.out.print("Choose an option: ");
             int choice = scanner.nextInt();
 
-            if (choice == 1 || choice == 2) {
+            if (choice >= 1 && choice <= 4) {
                 System.out.print("Enter your bet amount: $");
                 double betAmount = scanner.nextDouble();
 
@@ -29,34 +31,53 @@ public class craps2 {
 
                 int point = -1;
                 int sum = rollDice(random);
-                String betType = (choice == 1) ? "Pass" : "Don't Pass";
+                String betType = "";
+
+                switch (choice) {
+                    case 1:
+                        betType = "Pass";
+                        break;
+                    case 2:
+                        betType = "Don't Pass";
+                        break;
+                    case 3:
+                        betType = "Come";
+                        break;
+                    case 4:
+                        betType = "Don't Come";
+                        break;
+                }
+
                 System.out.println("Initial roll: " + sum);
 
-                if (sum == 7 || sum == 11) {
-                    System.out.println("You win on " + betType);
-                    balance += betAmount;
-                } else if (sum == 2 || sum == 3 || sum == 12) {
-                    System.out.println("You lose on " + betType);
-                    balance -= betAmount;
-                } else {
-                    point = sum;
-                    System.out.println("Point is " + point);
-                }
-
-                while (point != -1) {
-                    sum = rollDice(random);
-                    System.out.println("Next roll: " + sum);
-                    if (sum == 7) {
-                        System.out.println("You lose on " + betType);
-                        balance -= betAmount;
-                        break;
-                    } else if (sum == point) {
+                if (choice == 1 || choice == 3) {
+                    if (sum == 7 || sum == 11) {
                         System.out.println("You win on " + betType);
                         balance += betAmount;
-                        break;
+                    } else if (sum == 2 || sum == 3 || sum == 12) {
+                        System.out.println("You lose on " + betType);
+                        balance -= betAmount;
+                    } else {
+                        point = sum;
+                        System.out.println("Point is " + point);
+                    }
+                } else if (choice == 2 || choice == 4) {
+                    if (sum == 7 || sum == 11) {
+                        System.out.println("You lose on " + betType);
+                        balance -= betAmount;
+                    } else if (sum == 2 || sum == 3) {
+                        System.out.println("You win on " + betType);
+                        balance += betAmount;
+                    } else if (sum == 12) {
+                        System.out.println("It's a push on " + betType);
+                    } else {
+                        point = sum;
+                        System.out.println("Point is " + point);
                     }
                 }
-            } else if (choice == 3) {
+                // NEED MORE LOGIC HERE
+
+            } else if (choice == 5) {
                 continuePlaying = false;
                 System.out.println("Thank you for playing!");
             } else {
