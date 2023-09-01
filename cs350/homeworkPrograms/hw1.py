8bb24aba8145f60df3b56839b628516708be2d25
zachhealy <zachjhealy@gmail.com>
2023-09-01 17:30:00 -0500

    commit on 'main'

--- cs350/homeworkPrograms/hw1.py
+++ cs350/homeworkPrograms/hw1.py
@@ -16,8 +16,19 @@ twofive = 1
 
 while twofive > five:
     n+=1
-    twofive = 25 * n * math.log(n)
+    twofive = 25 * n * math.log2(n)
     five = 5 * (n ** 2)
     print(n)
 '''
 
+n = 1
+seven = 0
+fifty = 1
+
+while fifty > seven:
+    n+=1
+    fifty = 50 * n * math.log2(n)
+    seven = 7 * (n ** 2)
+    print(n)
+    
+    
--- cs390/java/craps.java
+++ cs390/java/craps.java
@@ -3,39 +3,104 @@ package cs390.java;
 import java.util.*;
 
 public class craps {
+
     public static void main(String[] args) {
-        int x = 1;
         Scanner input = new Scanner(System.in);
-        User user = new User();
+        Random random = new Random();
+        double balance = 100.0;
+        boolean continuePlaying = true;
 
-        while (x != 0) {
-            System.out.println();
-            System.out.println("Account Balance: " + user.balance);
-            System.out.println("--");
-            System.out.println("1 - Bet");
-            System.out.println("0 - Exit Game");
-            System.out.print("Please enter a number: ");
-            x = input.nextInt();
+        while (continuePlaying) {
+            System.out.println("Balance: $" + balance);
+            System.out.println("1. Bet on Pass");
+            System.out.println("2. Bet on Don't Pass");
+            System.out.println("3. Bet on Come");
+            System.out.println("4. Bet on Don't Come");
+            System.out.println("5. Quit");
+            System.out.print("Choose an option: ");
+            int choice = input.nextInt();
 
-            switch (x) {
-                case 1:
-                    diceRoll();
-                    break;
+            if (choice >= 1 && choice <= 4) {
+                System.out.print("Enter your bet amount: $");
+                double betAmount = input.nextDouble();
 
-                default:
-                    System.out.println("error, returning to menu");
-                    break;
+                if (betAmount > balance) {
+                    System.out.println("Insufficient balance for this bet.");
+                    continue;
+                }
+
+                balance -= betAmount;
+
+                int point = -1;
+                int sum = rollDice(random);
+                String betType = "";
+
+                switch (choice) {
+                    case 1:
+                        betType = "Pass";
+                        break;
+                    case 2:
+                        betType = "Don't Pass";
+                        break;
+                    case 3:
+                        betType = "Come";
+                        break;
+                    case 4:
+                        betType = "Don't Come";
+                        break;
+                }
+
+                System.out.println("Initial roll: " + sum);
+
+                if (choice == 1 || choice == 3) {
+                    if (sum == 7 || sum == 11) {
+                        System.out.println("You win on " + betType);
+                        balance += betAmount * 2;
+                    } else if (sum == 2 || sum == 3 || sum == 12) {
+                        System.out.println("You lose on " + betType);
+
+                    } else {
+                        point = sum;
+                        System.out.println("Point is " + point);
+                    }
+                } else if (choice == 2 || choice == 4) {
+                    if (sum == 7 || sum == 11) {
+                        System.out.println("You lose on " + betType);
+
+                    } else if (sum == 2 || sum == 3) {
+                        System.out.println("You win on " + betType);
+                        balance += betAmount * 2;
+                    } else if (sum == 12) {
+                        System.out.println("It's a push on " + betType);
+                    } else {
+                        point = sum;
+                        System.out.println("Point is " + point);
+                    }
+                }
+
+            } else if (choice == 5) {
+                continuePlaying = false;
+                System.out.println("Thank you for playing!");
+            } else {
+                System.out.println("Invalid choice. Please choose a valid option.");
+            }
+
+            if (balance <= 0) {
+                System.out.println("You are out of balance. Game over.");
+                break;
             }
         }
     }
 
-    public static void diceRoll() {
-        Random rand = new Random();
+    private static int rollDice(Random random) {
+        int dieRolls[];
+        dieRolls = new int[2];
+        for (int i = 0; i < 2; i++) {
+            dieRolls[i] = random.nextInt(6) + 1;
+        }
 
         for (int i = 0; i < 2; i++) {
-            int roll = rand.nextInt(6) + 1;
-
-            switch (roll) {
+            switch (dieRolls[i]) {
                 case 1:
                     System.out.println("--------");
                     System.out.println("|       |");
@@ -83,14 +148,6 @@ public class craps {
                     break;
             }
         }
-
-    }
-
-    static class User {
-        public double balance;
-
-        public User() {
-            balance = 500.00;
-        }
+        return dieRolls[0] + dieRolls[1];
     }
 }
