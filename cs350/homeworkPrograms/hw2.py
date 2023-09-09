30d4a5aac176f8dc54919e7b06b8b02ce49f19de
Zach Healy <zachjhealy@gmail.com>
2023-09-09 13:43:38 -0500

    Create hw2.py

---
+++ cs350/homeworkPrograms/hw2.py
@@ -0,0 +1,63 @@
+import numpy as np
+
+'''
+number 1
+----------
+
+def arrayMax(a, n):
+    currentMax = a[0]
+    
+    for i in range(n - 1):
+        if a[i] > currentMax:
+            currentMax = a[i]
+        n =+ 1
+    return currentMax
+
+n = 100
+a = np.random.randint(0, 100, 100)
+
+print(arrayMax(a, n))
+'''
+
+
+
+
+'''
+number 4
+----------
+
+n = 100
+i = 1
+sum = 0
+
+while i <= n:
+    j = 1
+    while j <= n:
+        sum = sum + i
+        j += 1
+    i += 1
+'''
+
+'''
+number 5
+---------
+
+n = 100
+i = 1
+sum = 0
+
+while i <- n:
+    i =+ 1
+    sum += i
+
+'''
+
+'''
+number 6
+----------
+
+
+n = 100
+i = 
+
+'''
